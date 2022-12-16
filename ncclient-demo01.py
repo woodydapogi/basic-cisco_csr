d28ea4dda7211f5a16a988b582ad999a6d372342
woodydapogi <21982601+woodydapogi@users.noreply.github.com>
2022-12-16 12:14:36 +0800

    Update ncclient-demo01.py

--- ncclient-demo01.py
+++ ncclient-demo01.py
@@ -1,1 +1,25 @@
+#!/usr/bin/python3
 
+try:
+    from ncclient import manager as ma
+
+except ImportError as i_err:
+    print(i_err)
+
+def main():
+  '''using with for the connection to be to close automatically.'''
+  #details
+  with ma.connect(
+    host="host1.woodydapogi.local",
+    port=22,
+    username="user1",
+    password="password123",
+    hostkey_verify=False,
+    device_params={'name': 'csr'}) as m:
+    
+    #printing device's capabilities
+    for dev in m.server_capabilities:
+      print(dev)
+
+main()
+      
